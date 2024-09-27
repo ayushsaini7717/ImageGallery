657e91899df1d9f7fa87f9ddb8616b01550c955e
ayushsaini7717 <ayushsaini7717@gmail.com>
2024-09-27 14:28:56 +0530

    template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import Template from './components/template';
+import Template from './components/Template/';
 
 
 function App() {
